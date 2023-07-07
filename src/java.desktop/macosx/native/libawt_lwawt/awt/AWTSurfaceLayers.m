c188af564113dd693086c369eb5e4392246b8ca5
Jazzzny <jazzzny225@gmail.com>
2023-07-07 12:56:52 -0400

    Revert "8260931: Implement JEP 382: New macOS Rendering Pipeline"
    This reverts commit 8afec70c283ee549795996031e3a53a3212bf35a.
    
    # Conflicts:
    #	src/java.desktop/macosx/classes/sun/awt/CGraphicsDevice.java
    #	src/java.desktop/macosx/classes/sun/java2d/MacOSFlags.java
    #	src/java.desktop/macosx/classes/sun/java2d/metal/MTLContext.java
    #	src/java.desktop/macosx/classes/sun/java2d/metal/MTLGraphicsConfig.java
    #	src/java.desktop/macosx/classes/sun/java2d/metal/MTLRenderQueue.java
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/EncoderManager.m
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLBlitLoops.m
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLClip.h
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLClip.m
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLGraphicsConfig.h
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLGraphicsConfig.m
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLLayer.m
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLPipelineStatesStorage.m
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLRenderQueue.m
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLRenderer.m
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLStencilManager.h
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLStencilManager.m
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLSurfaceData.m
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLSurfaceDataBase.h
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLTextRenderer.m
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/MTLTexurePool.m
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/common.h
    #	src/java.desktop/macosx/native/libawt_lwawt/java2d/metal/shaders.metal

--- src/java.desktop/macosx/native/libawt_lwawt/awt/AWTSurfaceLayers.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/AWTSurfaceLayers.m
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -29,7 +29,6 @@
 #import "JNIUtilities.h"
 
 #import <QuartzCore/CATransaction.h>
-#import <QuartzCore/CAMetalLayer.h>
 
 @implementation AWTSurfaceLayers
 
@@ -68,11 +67,10 @@
     }
 }
 
-// Updates back buffer size of the layer if it's an OpenGL/Metal layer
-// including all OpenGL/Metal sublayers
+// Updates back buffer size of the layer if it's an OpenGL layer
+// including all OpenGL sublayers
 + (void) repaintLayersRecursively:(CALayer*)aLayer {
-    if ([aLayer isKindOfClass:[CAOpenGLLayer class]] ||
-        [aLayer isKindOfClass:[CAMetalLayer class]]) {
+    if ([aLayer isKindOfClass:[CAOpenGLLayer class]]) {
         [aLayer setNeedsDisplay];
     }
     for(CALayer *child in aLayer.sublayers) {
@@ -94,7 +92,7 @@
 
 /*
  * Class:     sun_lwawt_macosx_CPlatformComponent
- * Method:    nativeCreateComponent
+ * Method:    nativeCreateLayer
  * Signature: ()J
  */
 JNIEXPORT jlong JNICALL
